ca4d62ba50e19d8ac2861240d02bc4e3effd6253
oudaming <oudaming@lansent.com>
2020-12-30 18:56:07 +0800

    螺旋矩阵

---
+++ src/main/java/cn/leetcode/_54SpiralMatrix.java
@@ -0,0 +1,142 @@
+package cn.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 54.螺旋矩阵
+ * <p>
+ * 给定一个包含m x n个元素的矩阵（m 行, n 列），请按照顺时针螺旋顺序，返回矩阵中的所有元素。
+ * <p>
+ * 示例1:
+ * <p>
+ * 输入:
+ * [
+ * [ 1, 2, 3 ],
+ * [ 4, 5, 6 ],
+ * [ 7, 8, 9 ]
+ * ]
+ * 输出: [1,2,3,6,9,8,7,4,5]
+ * <p>
+ * <p>
+ * 思路：0:向左走，1:向下走，2:向左走，3:向上走
+ * 使用一个int[][]数组记录走过的路，走过标记为 -1
+ * 当走到头或走到已经标记走过的边界时，通过 (++i % 4) 改变方向即可
+ *
+ * @author oudaming
+ * @date 2020-12-30 17:42
+ */
+public class _54SpiralMatrix {
+
+
+    public static void main(String[] args) {
+//        int[][] matrix = new int[4][3];
+//        matrix[0] = new int[]{19, 12, 45};
+//        matrix[1] = new int[]{23, 1, 67};
+//        matrix[2] = new int[]{50, 34, 0};
+//        matrix[3] = new int[]{9, 8, 7};
+//        int[][] matrix = new int[2][1];
+//        matrix[0] = new int[]{1};
+//        matrix[1] = new int[]{2};
+        int[][] matrix = new int[2][1];
+        matrix[0] = new int[]{1};
+        matrix[1] = new int[]{3};
+        List<Integer> integers = new _54SpiralMatrix().spiralOrder(matrix);
+        integers.forEach(System.out::println);
+    }
+
+    public List<Integer> spiralOrder(int[][] matrix) {
+        int direction = 0;
+        int[][] visit = new int[matrix.length][matrix[0].length];
+        List<Integer> result = new ArrayList<>();
+
+        int row = 0; // 行
+        int col = 0; // 列
+        // 先从第一行第一列开始
+        result.add(matrix[row][col]);
+        visit[row][col] = -1;
+
+        if (matrix.length == 1 && matrix[0].length == 1) {
+            return result;
+        }
+
+        while (true) {
+            if (direction == 0) {
+                if (col + 1 != matrix[0].length && visit[row][col + 1] == -1) {
+                    return result;
+                }
+                if (col + 1 == matrix[0].length) {
+                    direction = (direction + 1) % 4;
+                } else {
+                    while (true) {
+                        if (col + 1 < matrix[0].length && visit[row][col + 1] != -1) {
+                            result.add(matrix[row][++col]);
+                            visit[row][col] = -1;
+                        } else {
+                            direction = (direction + 1) % 4;
+                            break;
+                        }
+                    }
+                }
+
+            }
+            if (direction == 1) {
+                if (row + 1 != matrix.length && visit[row + 1][col] == -1) {
+                    return result;
+                }
+                if (row + 1 == matrix.length) {
+                    direction = (direction + 1) % 4;
+                } else {
+                    while (true) {
+                        if (row + 1 < matrix.length && visit[row + 1][col] != -1) {
+                            result.add(matrix[++row][col]);
+                            visit[row][col] = -1;
+                        } else {
+                            direction = (direction + 1) % 4;
+                            break;
+                        }
+                    }
+                }
+
+            }
+            if (direction == 2) {
+                if (col - 1 >= 0 && visit[row][col - 1] == -1) {
+                    return result;
+                }
+                if (col - 1 < 0) {
+                    direction = (direction + 1) % 4;
+                } else {
+                    while (true) {
+                        if (col - 1 >= 0 && visit[row][col - 1] != -1) {
+                            result.add(matrix[row][--col]);
+                            visit[row][col] = -1;
+                        } else {
+                            direction = (direction + 1) % 4;
+                            break;
+                        }
+                    }
+                }
+
+            }
+            if (direction == 3) {
+                if (row - 1 >= 0 && visit[row - 1][col] == -1) {
+                    return result;
+                }
+                if (row - 1 < 0) {
+                    direction = (direction + 1) % 4;
+                } else {
+                    while (true) {
+                        if (row - 1 >= 0 && visit[row - 1][col] != -1) {
+                            result.add(matrix[--row][col]);
+                            visit[row][col] = -1;
+                        } else {
+                            direction = (direction + 1) % 4;
+                            break;
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+}
