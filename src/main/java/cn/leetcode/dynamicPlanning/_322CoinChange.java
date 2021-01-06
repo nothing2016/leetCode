4c5f84610048dfd90ef0029c0cde939aaff9f8f3
oudaming <805269294@qq.com>
2021-01-06 22:29:10 +0800

    零钱兑换

--- src/main/java/cn/leetcode/dynamicPlanning/_322CoinChange.java
+++ src/main/java/cn/leetcode/dynamicPlanning/_322CoinChange.java
@@ -1,9 +1,6 @@
 package cn.leetcode.dynamicPlanning;
 
-import java.util.stream.IntStream;
-
 /**
- * TODO   目前还没有找到解
  * 322. 零钱兑换
  * 给定不同面额的硬币 coins 和一个总金额 amount。编写一个函数来计算可以凑成总金额所需的最少的硬币个数。如果没有任何一种硬币组合能组成总金额，返回-1。
  * <p>
@@ -18,44 +15,81 @@ import java.util.stream.IntStream;
  */
 public class _322CoinChange {
     public int coinChange(int[] coins, int amount) {
-        if (coins == null || coins.length == 0 || amount == 0) {
-            return 0;
+        if (coins == null || coins.length == 0 || amount < 0) {
+            return -1;
         }
-        // 数组逆序，贪心算法
-        coins = IntStream.of(coins).boxed().sorted((a, b) -> b - a).mapToInt(Integer::intValue).toArray();
-        int number = getNumber(coins, 0, amount);
-        return number == 0 ? -1 : number;
+        return process(coins, 0, amount);
     }
 
     /**
-     * @param coins
-     * @param index 当前要哪一个硬币的下标
-     * @param rest  要组成多少钱
+     * @param arr  数组
+     * @param i    当前来到的点
+     * @param rest 剩下的钱
      * @return
      */
-    public int getNumber(int[] coins, int index, int rest) {
-        if (rest < 0 || index >= coins.length) {
-            return 0;
+    public int process(int[] arr, int i, int rest) {
+        if (i == arr.length) {
+            // 刚好到最后时，如果为0，那么用0的面值组成0，使用的是0张
+            return rest == 0 ? 0 : -1;
         }
-        if (rest == 0) {
-            return index == coins.length ? 1 : 0;
-        }
-        // 使用最少的硬币个数
-        int sum = Integer.MAX_VALUE;
-        // i代表多少个coins[index]的硬币
-        for (int i = 0; i * coins[index] < rest; i++) {
-            int number = getNumber(coins, index + 1, rest - (coins[index] * i));
-            if (number >= 0) {
-                sum = Math.min(sum, number + i);
+        // 默认是没有找到任何解
+        int result = -1;
+        // 使用k张面值为arr[i]的零钱
+        for (int k = 0; k * arr[i] <= rest; k++) {
+            int next = process(arr, i + 1, rest - k * arr[i]);
+            // 后面的值有效的情况，无效默认是-1了
+            if (next != -1) {
+                result = result == -1 ? k + next : Math.min(result, k + next);
             }
         }
-        return sum == Integer.MAX_VALUE ? 0 : sum;
+        return result;
+    }
+
+    /**
+     * dp解法
+     *
+     * @param arr
+     * @param aim
+     * @return
+     */
+    public int coinChange2(int[] arr, int aim) {
+        if (arr == null || arr.length == 0 || aim < 0) {
+            return -1;
+        }
+        int[][] dp = new int[arr.length + 1][aim + 1];
+        // 初始化为-1
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < dp[0].length; j++) {
+                dp[i][j] = -1;
+            }
+        }
+        dp[arr.length][0] = 0;
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            for (int rest = 0; rest <= aim; rest++) {
+                // dp[i][rest - arr[i]] 的位置是有效的
+                if (rest - arr[i] >= 0 && dp[i][rest - arr[i]] != -1) {
+
+                    if (dp[i + 1][rest] == -1) {
+                        dp[i][rest] = dp[i][rest - arr[i]] + 1;
+                    } else {
+                        // dp[i][rest - arr[i]] 和dp[i + 1][rest] 的位置同时有效
+                        dp[i][rest] = Math.min(dp[i + 1][rest], dp[i][rest - arr[i]] + 1);
+                    }
+                } else {
+                    dp[i][rest] = dp[i + 1][rest];
+                }
+            }
+        }
+        return dp[0][aim];
     }
 
     public static void main(String[] args) {
         int[] coins = {1, 2, 5};
         int amount = 11;
         int sum = new _322CoinChange().coinChange(coins, amount);
+        int sum2 = new _322CoinChange().coinChange2(coins, amount);
         System.out.println(sum);
+        System.out.println(sum2);
     }
 }
