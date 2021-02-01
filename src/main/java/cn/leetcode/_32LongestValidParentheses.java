f2660244ae42db82b1218077cace2d9c3a50aac2
oudaming <oudaming@lansent.com>
2021-02-01 10:16:51 +0800

    32. 最长有效括号

--- src/main/java/cn/leetcode/_32LongestValidParentheses.java
+++ src/main/java/cn/leetcode/_32LongestValidParentheses.java
@@ -11,11 +11,23 @@ package cn.leetcode;
  * 输入：s = ")()())"
  * 输出：4
  * 解释：最长有效括号子串是 "()()"
+ * <p>
+ * 题解： 使用动态规划，dp[i] = dp[pre - 1] + dp[i - 1] + 2  (pre = i - dp[i - 1] - 1)
+ * pre表示跟 “)” 匹配的 “(” 的位置下标
  *
  * @author oudaming
  * @date 2021-01-29 17:48
  */
 public class _32LongestValidParentheses {
+    public static void main(String[] args) {
+        System.out.println(new _32LongestValidParentheses().longestValidParentheses(")()())"));
+        System.out.println(new _32LongestValidParentheses().longestValidParentheses("()"));
+        System.out.println(new _32LongestValidParentheses().longestValidParentheses("())"));
+        System.out.println(new _32LongestValidParentheses().longestValidParentheses("("));
+        System.out.println(new _32LongestValidParentheses().longestValidParentheses(")(((()()()))"));
+        System.out.println(new _32LongestValidParentheses().longestValidParentheses(""));
+    }
+
     public int longestValidParentheses(String s) {
         if (s == null || s.length() == 0) {
             return 0;
@@ -24,17 +36,26 @@ public class _32LongestValidParentheses {
         // dp代表记录以当前i下标的值为最后一个字符，得到的最大有效的长度
         int[] dp = new int[len];
         char[] chars = s.toCharArray();
+        int max = 0;
         // 002040
         // )()())
         // 012345
+        // )(((()()()) )
+        // 0000020406810
         for (int i = 1; i < len; i++) {
             if (chars[i] == ')') {
-                if (i - dp[i - 1] - 1 >= 0) {
-
+                // 上一个匹配的位置
+                int pre = i - dp[i - 1] - 1;
+                if (pre >= 0 && chars[pre] == '(') {
+                    if (pre - 1 >= 0) {
+                        dp[i] = dp[pre - 1] + dp[i - 1] + 2;
+                    } else {
+                        dp[i] = dp[i - 1] + 2;
+                    }
+                    max = Math.max(max, dp[i]);
                 }
-                dp[i] = dp[i - dp[i - 1] - 1];
             }
         }
-        return 0;
+        return max;
     }
 }
