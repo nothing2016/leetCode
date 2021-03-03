eabc8a9a5646326284ed893316daa5ecefafcdf0
oudaming <oudaming@lansent.com>
2021-03-03 10:03:06 +0800

    98. 验证二叉搜索树

--- src/main/java/cn/leetcode/_98_Validate_Binary_Search_Tree.java
+++ src/main/java/cn/leetcode/_98_Validate_Binary_Search_Tree.java
@@ -13,13 +13,15 @@ public class _98_Validate_Binary_Search_Tree {
     public static void main(String[] args) {
         TreeNode node1 = new TreeNode(5);
         TreeNode node2 = new TreeNode(4);
-        TreeNode node3 = new TreeNode(6);
-        TreeNode node4 = new TreeNode(3);
-        TreeNode node5 = new TreeNode(7);
+        TreeNode node3 = new TreeNode(7);
+        TreeNode node4 = new TreeNode(6);
+        TreeNode node5 = new TreeNode(8);
         node1.left = node2;
         node1.right = node3;
         node3.left = node4;
         node3.right = node5;
+
+        TreeNode node = new TreeNode(2147483647);
         System.out.println(new _98_Validate_Binary_Search_Tree().isValidBST(node1));
 //        System.out.println(new _98_Validate_Binary_Search_Tree().isValidBST(null));
     }
@@ -31,30 +33,35 @@ public class _98_Validate_Binary_Search_Tree {
 
 
     public Info process(TreeNode root) {
-        return null;
-//        if (root == null) {
-//            return ;
-//        }
-//        TreeNode left = root.left;
-//        TreeNode right = root.right;
-//        if (left != null && left.val >= root.val) {
-//            return false;
-//        }
-//        if (right != null && right.val <= root.val) {
-//            return false;
-//        }
-//        return isValidBST(root.left) && isValidBST(root.right);
+        if (root == null) {
+            return new Info(null, null, true);
+        }
+        Info leftInfo = process(root.left);
+        Info rightInfo = process(root.right);
+
+        if (leftInfo.isOk && rightInfo.isOk && (leftInfo.max == null || root.val > leftInfo.max) && (rightInfo.min == null || root.val < rightInfo.min)) {
+            int max = rightInfo.max == null ? root.val : rightInfo.max;
+            int min = leftInfo.min == null ? root.val : leftInfo.min;
+            return new Info(max, min, true);
+        } else {
+            return new Info(null, null, false);
+        }
     }
 
     private static class Info {
-        int max;
-        int min;
+        // 整个树上的最大值
+        Integer max;
+        // 整个树上的最小值
+        Integer min;
+        // 是否是搜索二叉树
         boolean isOk;
 
-        public void info(int max, int min, boolean isOk) {
+        public Info(Integer max, Integer min, boolean isOk) {
             this.max = max;
             this.min = min;
             this.isOk = isOk;
         }
     }
+
 }
+
