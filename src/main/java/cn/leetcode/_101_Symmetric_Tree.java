063f6c70d965b5451cb240548d6f3deb88edb524
oudaming <oudaming@lansent.com>
2021-03-03 14:07:39 +0800

    101. 对称二叉树

--- src/main/java/cn/leetcode/_101_Symmetric_Tree.java
+++ src/main/java/cn/leetcode/_101_Symmetric_Tree.java
@@ -101,6 +101,12 @@ public class _101_Symmetric_Tree {
         System.out.println(symmetric);
     }
 
+    /**
+     * 使用的是宽度优先遍历，再使用stack计算每一层是否是对称
+     *
+     * @param root
+     * @return
+     */
     public boolean isSymmetric(TreeNode root) {
         if (root == null) {
             return true;
@@ -118,7 +124,7 @@ public class _101_Symmetric_Tree {
         while (!queue.isEmpty()) {
             TreeNode poll = queue.poll();
 
-            System.out.print(poll.val + ",");
+//            System.out.print(poll.val + ",");
 
             if (!stack.isEmpty() && stack.peek() == poll.val && isLeftMap.getOrDefault(poll, 1) == 0) {
                 stack.pop();
@@ -142,5 +148,32 @@ public class _101_Symmetric_Tree {
             }
         }
         return stack.isEmpty();
+    }
+
+    public boolean isSymmetric2(TreeNode root) {
+        return isMirror(root, root);
+    }
+
+    // 一棵树是原始树  head1
+    // 另一棵是翻面树  head2
+
+    /**
+     * 递归判断两颗树是否是对称二叉树
+     *
+     * @param head1
+     * @param head2
+     * @return
+     */
+    public static boolean isMirror(TreeNode head1, TreeNode head2) {
+        if (head1 == null && head2 == null) {
+            return true;
+        }
+        if (head1 != null && head2 != null) {
+            return head1.val == head2.val
+                    && isMirror(head1.left, head2.right)
+                    && isMirror(head1.right, head2.left);
+        }
+        // 一个为空，一个不为空  false
+        return false;
     }
 }
