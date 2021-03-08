482c1ac4fc4a0281573d1ee74cbf1cc32ea75c4c
oudaming <oudaming@lansent.com>
2021-03-08 10:04:26 +0800

    138. 复制带随机指针的链表

---
+++ src/main/java/cn/leetcode/_138_Copy_List_with_Random_Pointer.java
@@ -0,0 +1,56 @@
+package cn.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 138. 复制带随机指针的链表
+ * <p>
+ * 给你一个长度为 n 的链表，每个节点包含一个额外增加的随机指针 random ，该指针可以指向链表中的任何节点或空节点。
+ * <p>
+ * 构造这个链表的深拷贝。深拷贝应该正好由 n 个 全新 节点组成，其中每个新节点的值都设为其对应的原节点的值。新节点的 next 指针和 random 指针也都应指向复制链表中的新节点，
+ * 并使原链表和复制链表中的这些指针能够表示相同的链表状态。复制链表中的指针都不应指向原链表中的节点 。
+ * 例如，如果原链表中有 X 和 Y 两个节点，其中 X.random --> Y 。那么在复制链表中对应的两个节点 x 和 y ，同样有 x.random --> y 。
+ * 返回复制链表的头节点。
+ *
+ * @author oudaming
+ * @date 2021-03-08 9:53
+ */
+public class _138_Copy_List_with_Random_Pointer {
+    public Node copyRandomList(Node head) {
+        if (head == null) {
+            return null;
+        }
+        // 新原节点对应克隆节点的映射
+        Map<Node, Node> map = new HashMap<>();
+        Node root = head;
+        while (head != null) {
+            map.put(head, new Node(head.val));
+            head = head.next;
+        }
+
+        head = root;
+        while (head != null) {
+            Node newNode = map.get(head);
+            Node newRandom = head.random == null ? null : map.get(head.random);
+            Node newNext = head.next == null ? null : map.get(head.next);
+            newNode.random = newRandom;
+            newNode.next = newNext;
+            head = head.next;
+        }
+        return map.get(root);
+
+    }
+
+    private static class Node {
+        int val;
+        Node next;
+        Node random;
+
+        public Node(int val) {
+            this.val = val;
+            this.next = null;
+            this.random = null;
+        }
+    }
+}
